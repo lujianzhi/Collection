70d921cfaf9e0c864e96b665a4036bc51f6d0444
lujianzhi <lujianzhixiaoyao@163.com>
2018-01-24 11:42:29 +0800

    创建项目

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ian.Lu on 2018/1/24.
+ * Project : Collection
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Hello Collection");
+    }
+}
