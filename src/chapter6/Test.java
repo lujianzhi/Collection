c4a436915683c5769a8ad9c160d5b069dd655de9
lujianzhi <lujianzhixiaoyao@163.com>
2018-02-04 15:16:49 +0800

    Vector源码分析

--- src/chapter6/Test.java
+++ src/chapter6/Test.java
@@ -9,7 +9,34 @@ import java.util.Vector;
 public class Test {
 
     public static void main(String[] args) {
+//        test();
 
+        analyzeCode();
+    }
+
+    private static void analyzeCode() {
+        Vector<String> vector = new Vector<>();
+
+        vector.add("第1");
+        vector.add("第2");
+        vector.add("第3");
+        vector.add("第4");
+        vector.add("第5");
+        vector.add("第6");
+        vector.add("第7");
+        vector.add("第8");
+        vector.add("第9");
+        vector.add("第10");
+        vector.add("第11");
+
+        vector.remove(2);
+
+        vector.get(2);
+
+        vector.set(2, "将index=2的值替换");
+    }
+
+    private static void test() {
         Vector<String> vector = new Vector<>();
 
         vector.add("第1");
@@ -27,7 +54,6 @@ public class Test {
         printAll(vector);
 
         System.out.println("总容量:" + vector.capacity());
-
     }
 
     private static void printAll(Vector<String> vector) {
